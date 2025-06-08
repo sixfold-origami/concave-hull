44f20aedd3cfc3cce14a508e7d537203e8feb97b
sixfold <rosepeck1997@gmail.com>
2025-06-08 10:30:00 -0700

    Add CLI stub

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,30 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+/// Basic CLI to interface with the concave hull library
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Cli {
+    /// Path to input file
+    pub input: String,
+
+    /// Concavity parameter to use
+    pub concavity: f32,
+
+    /// Path to output polyline points to
+    #[arg(short, long, default_value = "output.csv")]
+    pub output: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let input = PathBuf::from(args.input);
+    let _output = PathBuf::from(args.output);
+
+    println!(
+        "Generating concave hull for {} [concavity: {}]",
+        input.display(),
+        args.concavity
+    );
 }
