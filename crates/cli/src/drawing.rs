88f7ef89069a76dcd56c2487ab1f3f59810aee80
sixfold <rosepeck1997@gmail.com>
2025-06-08 14:31:23 -0700

    Add image output
    There doesn't seem to be a way to make the line segments thicker, but this will do

---
+++ crates/cli/src/drawing.rs
@@ -0,0 +1,51 @@
+use concave_hull::{
+    Edge, Point,
+    parry2d::bounding_volume::{BoundingVolume, details::local_point_cloud_aabb},
+};
+use imageproc::{
+    drawing::{draw_filled_circle_mut, draw_line_segment_mut},
+    image::{Rgb, RgbImage},
+};
+
+/// Padding added to each side of the image, so that points aren't right up against the edge of the canvas
+///
+/// Stored as an f32 because it makes it slightly more convenient
+const IMG_PADDING: f32 = 10.;
+
+const POINT_COLOR: Rgb<u8> = Rgb([255u8, 255u8, 255u8]);
+const SEGMENT_COLOR: Rgb<u8> = Rgb([255u8, 0u8, 0u8]);
+
+pub fn draw_points_and_hull(points: &[Point], hull: &[Edge]) -> RgbImage {
+    let aabb = local_point_cloud_aabb(points);
+    let canvas = aabb.loosened(IMG_PADDING);
+    let point_size = (aabb.extents().max() / 250.).max(2.) as i32;
+    // let line_width = (aabb.extents().max() / 500.).max(2.) as u32;
+
+    let mut image = RgbImage::new(canvas.extents().x as u32, canvas.extents().y as u32);
+
+    for point in points {
+        let point = point - canvas.mins;
+        draw_filled_circle_mut(
+            &mut image,
+            (point.x as i32, point.y as i32),
+            point_size,
+            POINT_COLOR,
+        );
+    }
+
+    for edge in hull {
+        let a = edge.segment.a - canvas.mins;
+        let b = edge.segment.b - canvas.mins;
+
+        draw_filled_circle_mut(
+            &mut image,
+            (a.x as i32, a.y as i32),
+            point_size,
+            SEGMENT_COLOR,
+        );
+
+        draw_line_segment_mut(&mut image, (a.x, a.y), (b.x, b.y), SEGMENT_COLOR);
+    }
+
+    image
+}
--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -5,29 +5,38 @@ use clap::Parser;
 use concave_hull::{Point, concave_hull};
 use csv::{ReaderBuilder, Writer};
 
+use crate::drawing::draw_points_and_hull;
+
+mod drawing;
+
 /// Basic CLI to interface with the concave hull library
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Cli {
-    /// Path to input CSV file, with an x column and y column (in order)
-    input: String,
-
     /// Concavity parameter to use
     concavity: f32,
 
-    /// Path to output hull points to
-    #[arg(short, long, default_value = "output.csv")]
-    output: String,
+    /// Path to input CSV file, with an x column and y column (in order)
+    input: String,
+
+    /// Path to output a CSV of hull points to
+    #[arg(short, long)]
+    point_output: Option<String>,
+
+    /// Path to output a PNG image of the points and hull to
+    #[arg(short, long)]
+    img_output: Option<String>,
 
     /// Whether the input CSV has headers
-    #[arg(short, long, default_value_t = true)]
+    #[arg(short, long, default_value_t = false)]
     headers: bool,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
     let input = PathBuf::from(args.input);
-    let output = PathBuf::from(args.output);
+    let point_output = args.point_output.map(|path| PathBuf::from(path));
+    let img_output = args.img_output.map(|path| PathBuf::from(path));
 
     println!(
         "Generating concave hull for {} [concavity: {}]",
@@ -56,9 +65,30 @@ fn main() -> anyhow::Result<()> {
     let hull = concave_hull(&in_points, args.concavity);
 
     // Output
-    let mut writer = Writer::from_path(output)?;
-    for edge in hull {
-        writer.write_record(&[edge.segment.a.x.to_string(), edge.segment.a.y.to_string()])?
+    if point_output.is_none() && img_output.is_none() {
+        println!("No output provided. Terminating.");
+    }
+
+    if let Some(point_output) = point_output {
+        println!(
+            "Writing concave hull points to {:?}",
+            point_output.display()
+        );
+
+        let mut writer = Writer::from_path(point_output)?;
+        for edge in hull.iter() {
+            writer.write_record(&[edge.segment.a.x.to_string(), edge.segment.a.y.to_string()])?
+        }
+    }
+
+    if let Some(img_output) = img_output {
+        println!(
+            "Drawing image of points and hull at {:?}",
+            img_output.display()
+        );
+
+        let image = draw_points_and_hull(&in_points, &hull);
+        image.save(img_output)?;
     }
 
     Ok(())
