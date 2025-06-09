d465b9e440995877b2537a3b9bf257b0647eb827
sixfold <rosepeck1997@gmail.com>
2025-06-08 23:30:40 -0700

    Add benches

--- TODO.md
+++ TODO.md
@@ -3,7 +3,6 @@
 - Compat features for crates other than nalgebra (`glam`, `geo`, that one compat crate (`mint`?))
 - Handle f32/f64 versions of `parry` (we should probably have two mirrored crates, like `parry`/`nalgebra` do)
 - Tests
-- Benches
 - Compare to `geo`'s impl
 - Use a non-zero f32 for the concavity parameter?
 - Make the two point degenerate case not panic, and add a test for it
---
+++ benches/bench.rs
@@ -0,0 +1,45 @@
+use std::{fs::File, time::Duration};
+
+use concave_hull::{Point, concave_hull};
+use criterion::{Criterion, criterion_group, criterion_main};
+use csv::ReaderBuilder;
+
+fn load_data(path: &str) -> Vec<Point> {
+    let f = File::open(path).unwrap();
+
+    let mut reader = ReaderBuilder::new().has_headers(false).from_reader(f);
+
+    reader
+        .records()
+        .map(|r| {
+            let r = r.unwrap();
+            let x = r[0].parse().unwrap();
+            let y = r[1].parse().unwrap();
+
+            Point::new(x, y)
+        })
+        .collect()
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("benches");
+    group
+        .measurement_time(Duration::from_secs_f32(30.))
+        .sample_size(1000);
+
+    let polygon = load_data("./test_data/polygon.csv");
+    group.bench_function("polygon", |b| b.iter(|| concave_hull(&polygon, 40.)));
+
+    let question_mark = load_data("./test_data/question_mark.csv");
+    group.bench_function("question mark", |b| {
+        b.iter(|| concave_hull(&question_mark, 40.))
+    });
+
+    let concaveman_1k = load_data("./test_data/concaveman_1k.csv");
+    group.bench_function("concaveman_1k", |b| {
+        b.iter(|| concave_hull(&concaveman_1k, 40.))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
